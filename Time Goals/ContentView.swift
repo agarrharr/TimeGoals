7b608bbf8947663bea367f886852e584029114d3
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-09-01 08:25:25 -0600

    Initial Commit

---
+++ Time Goals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time Goals
+//
+//  Created by Adam Garrett-Harris on 9/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
